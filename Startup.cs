335810c49ff4277e91d0d7f8c68c65387200bc88
Erik Lieben <erik@codetistic.com>
2016-03-05 09:51:51 +0100

    Added logging

--- Startup.cs
+++ Startup.cs
@@ -5,11 +5,25 @@ using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNet.StaticFiles;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetCorePushState
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        
+        public IConfigurationRoot Configuration { get; private set; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -17,8 +31,11 @@ namespace AspNetCorePushState
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            
             app.UseIISPlatformHandler();
             app.UseDefaultFiles();
             app.UseStaticFilesPushState();
@@ -33,9 +50,13 @@ namespace AspNetCorePushState
         private RequestDelegate next;
         private StaticFileMiddleware middleware;
         
+        private ILogger logger;
+        
         public PushStateStaticFileMiddleware(RequestDelegate next, 
         IHostingEnvironment hostingEnv, StaticFileOptions options, ILoggerFactory loggerFactory) 
         {
+            this.logger = loggerFactory.CreateLogger("PushState");
+            
             this.next = next;
             this.middleware = new StaticFileMiddleware(next, hostingEnv, options, loggerFactory);
             
@@ -45,8 +66,12 @@ namespace AspNetCorePushState
             
             this.middleware.Invoke(context).ContinueWith(task => {
                 if (context.Response.StatusCode == 404) {
+
                     context.Response.StatusCode = StatusCodes.Status200OK;
                     context.Request.Path = "/index.html";
+                    
+                    this.logger.LogWarning("Virtual route, redirecting to index.html");
+                    
                     this.middleware.Invoke(context);
                 }
             }).Wait();
